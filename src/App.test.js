bad33d62a342b1beef9d1a9acb89af6110f15868
Anup Bhandari <s10064884@student.bhtafe.edu.au>
2022-01-11 14:01:59 +1100

    navigation designed

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
+import Nav from "./Nav"
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
---
+++ src/Nav.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import "./Nav.css";
+import logo from "./icon.png";
+import userLogo from "./usericon.png";
+import Input from '@mui/material/Input';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@mui/icons-material/Search';
+
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import TvOutlinedIcon from '@mui/icons-material/TvOutlined';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import GroupsIcon from '@mui/icons-material/Groups';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+
+import DialpadIcon from '@mui/icons-material/Dialpad';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import {Chat} from '@mui/icons-material';
+
+
+export default function Nav() {
+    return (
+        <div className="nav">
+            <div className='nav__left'>
+                <img src={logo}
+                    className='logo'></img>
+                <Input id="input-with-icon-adornment" placeholder='Search facebook'
+                    startAdornment={
+                        <InputAdornment
+                    position="start" ><SearchIcon/></InputAdornment>
+                    }/>
+            </div>
+            <div className='nav__middle'>
+                <div className='nav__ico active'><HomeOutlinedIcon/></div>
+                <div className='nav__ico'><TvOutlinedIcon/></div>
+                <div className='nav__ico'><StorefrontIcon/></div>
+                <div className='nav__ico'><GroupsIcon/></div>
+                <div className='nav__ico'><SportsEsportsIcon/></div>
+            </div>
+            <div className='nav__right'>
+                <div className='nav__user'>
+                    <img src={userLogo}
+                        className='user_logo user__logo'></img>
+                    <span>Anup</span>
+                </div>
+                <div className='nav__control_ico'>
+                    <DialpadIcon/>
+                    <ChatBubbleIcon/>
+                    <NotificationsIcon/>
+                    <ArrowDropDownIcon/>
+                </div>
+            </div>
+        </div>
+    )
+}
